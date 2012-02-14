2e3c22094ce73e4889e866452ad7a60b9f99ce71
juanantonioruz <juanantonioruz@gmail.com>
2012-02-14 01:58:50 +0100

    spring session y jpa

--- src/com/enjava/web/Controlador.java
+++ src/com/enjava/web/Controlador.java
@@ -1,17 +1,42 @@
 package com.enjava.web;
 
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.enjava.Alumno;
+import com.enjava.EMF;
 
 @Controller
 public class Controlador {
+	@Autowired
+	Alumno alumno;
+
+	@Autowired
+	EMF emf;
 
 	
-    @RequestMapping("/helloWorld")
-    public String helloWorld(Model model) {
-        model.addAttribute("message", "Hello World!");
+    @RequestMapping(value="/helloW/nombre={nombre}")
+    public String helloWorld(@PathVariable() String nombre, Model model) {
+    	Alumno alumno=new Alumno();
+    	alumno.setNombre(nombre);
+    	EntityManager em = emf.getEM();
+    	em.getTransaction().begin();
+    	em.persist(alumno);
+    	em.getTransaction().commit();
+        model.addAttribute("message", "Hello World!"+alumno);
+        this.alumno.setNombre(alumno.getNombre());
+        
         return "helloWorld";
+    }
+    @RequestMapping(value="/helloW")
+    public String helloWorld( Model model) {
+    	model.addAttribute("message", "Hello World!"+alumno);
+    	return "helloWorld";
     }
 
 }
